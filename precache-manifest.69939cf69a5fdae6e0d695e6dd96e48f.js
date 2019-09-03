f459f0826fffa01f0069592e030782679e3d1f17
talk2coded <emmanuelsteve43@gmail.com>
2019-09-03 14:34:18 +0100

    Updates

---
+++ precache-manifest.69939cf69a5fdae6e0d695e6dd96e48f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ae986301003f86d0e2af771cb5e0211",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "da02f53f18c5fae751ba",
+    "url": "/monsters-rolodex/static/css/main.c74cc51d.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "da02f53f18c5fae751ba",
+    "url": "/monsters-rolodex/static/js/main.90ae3f06.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
