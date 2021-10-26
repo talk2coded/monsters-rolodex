0f16ac0eef0de71f79cbe6e6c4d813330f5faef8
Emmanuel Steve <emmanuelsteve43@gmail.com>
2021-10-26 14:17:46 +0100

    Updates

---
+++ precache-manifest.64276e474a988d630ec091f86f853991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52bcb214b750fe2f6a0c02199cee870f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b7c0b05964aa24284061",
+    "url": "/monsters-rolodex/static/css/main.613a301b.chunk.css"
+  },
+  {
+    "revision": "6fcd36c22064134da945",
+    "url": "/monsters-rolodex/static/js/2.76665908.chunk.js"
+  },
+  {
+    "revision": "b7c0b05964aa24284061",
+    "url": "/monsters-rolodex/static/js/main.c7155672.chunk.js"
+  },
+  {
+    "revision": "b7ad42eadca42871fc37",
+    "url": "/monsters-rolodex/static/js/runtime~main.d8f5eaa5.js"
+  }
+]);
